05196850ebbf321934ac69dcbca0cf0a078edeac
joaquin <joaquinruizperez2002@gmail.com>
2023-11-15 17:54:54 -0300

    Add SQL exercise

--- joaquin.sql
+++ joaquin.sql
@@ -1,8 +1,28 @@
 # GROUP BY nos sirve para agrupar registros sobre una columna
 
-SELECT genre_id, 
-COUNT(*) AS cantidad_peliculas, 
-SUM(awards) AS total_premios, 
-AVG(length) AS promedio_duracion
+# 1 
+SELECT COUNT(*) FROM movies;
+
+#2
+SELECT COUNT(*) FROM movies
+WHERE awards BETWEEN 3 AND 7;
+
+#3
+SELECT COUNT(*) FROM movies
+WHERE awards BETWEEN 3 AND 7 AND rating > 7;
+
+#4
+SELECT COUNT(*) 
+FROM actors_movie;
+
+#5
+SELECT genre_id, COUNT(*) 
 FROM movies
 GROUP BY genre_id;
+
+#6
+SELECT genre_id, awards, COUNT(*) 
+FROM movies
+GROUP BY genre_id
+
+
